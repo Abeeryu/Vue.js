0e0b8af6dd6763c72c36bb354419680f3ea7d962
Abeeryu <144008500+Abeeryu@users.noreply.github.com>
2025-11-02 02:36:07 +0100

    my first website

--- myproject/src/App.vue
+++ myproject/src/App.vue
@@ -1,26 +1,27 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Welcome to my website</h1>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+
+html, body, #app {
+  height: 100%;
+  margin: 0;
+  display: flex;
+  justify-content: center; 
+  align-items: center;     
+  
+}
+
+h1 {
+  font-family: sans-serif;
+  color: #1e40af;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
